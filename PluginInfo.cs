96d5c24bf27931026ca311f9faea33dbc4b5d3be
Lilsquishy15 <parker.james.krancki@gmail.com>
2025-06-29 10:27:35 +0100

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.PK.gorillatag.menutemplate";
+        public const string Name = "Vertex Menu";
+        public const string Description = "Created by PK with love <3";
+        public const string Version = "1.1.0";
+    }
+}
